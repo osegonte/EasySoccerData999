b8c7ba26066d5a4687f3fc2043847234ddff63f9
Manu <mandrescabral@gmail.com>
2025-03-07 18:04:23 -0300

    feat: add Player type and parser to Sofascore types

--- esd/sofascore/types/__init__.py
+++ esd/sofascore/types/__init__.py
@@ -5,5 +5,15 @@ Contains the types for the Sofascore service.
 from .event import Event, parse_events
 from .team import Team, parse_team
 from .team_ex import TeamEx, parse_team_ex
+from .player import Player, parse_player
 
-__all__ = ["Event", "parse_events", "Team", "parse_team", "TeamEx", "parse_team_ex"]
+__all__ = [
+    "Event",
+    "parse_events",
+    "Team",
+    "parse_team",
+    "TeamEx",
+    "parse_team_ex",
+    "Player",
+    "parse_player",
+]
