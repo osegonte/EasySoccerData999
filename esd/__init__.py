d28968ba67ef8d193936df197ff1a77bf7cd075d
Manu <mandrescabral@gmail.com>
2025-03-08 04:46:13 -0300

    feat: expose additional types in the EasySoccerData library

--- esd/__init__.py
+++ esd/__init__.py
@@ -2,7 +2,7 @@
 EasySoccerData - A Python easy-to-use library for soccer data analysis from multiple sources.
 """
 
-from .sofascore import SofascoreClient
+from .sofascore import SofascoreClient, EntityType, Event, Team, TeamEx, Player, MatchStats
 
-__all__ = ["SofascoreClient"]
+__all__ = ["SofascoreClient", "EntityType", "Event", "Team", "TeamEx", "Player", "MatchStats"]
 __version__ = "0.0.1"
--- esd/sofascore/__init__.py
+++ esd/sofascore/__init__.py
@@ -4,5 +4,14 @@ A submodule for extracting data from Sofascore.
 """
 
 from .client import SofascoreClient
+from .types import EntityType, Event, Team, TeamEx, Player, MatchStats
 
-__all__ = ["SofascoreClient"]
+__all__ = [
+    "SofascoreClient",
+    "EntityType",
+    "Event",
+    "Team",
+    "TeamEx",
+    "Player",
+    "MatchStats",
+]
