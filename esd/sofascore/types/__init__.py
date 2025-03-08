6c40ddae81018b2b198692675a8a373cfdaa7037
Manu <mandrescabral@gmail.com>
2025-03-08 03:18:43 -0300

    feat: add MatchStats and parse_match_stats to the public API

--- esd/sofascore/types/__init__.py
+++ esd/sofascore/types/__init__.py
@@ -6,6 +6,7 @@ from .event import Event, parse_events
 from .team import Team, parse_team
 from .team_ex import TeamEx, parse_team_ex
 from .player import Player, parse_player
+from .match_stats import MatchStats, parse_match_stats
 
 __all__ = [
     "Event",
@@ -16,4 +17,6 @@ __all__ = [
     "parse_team_ex",
     "Player",
     "parse_player",
+    "MatchStats",
+    "parse_match_stats",
 ]
