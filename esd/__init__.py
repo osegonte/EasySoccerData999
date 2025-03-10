5c524a668a971bae251bedf008e7f6246bb7fca3
Manu <mandrescabral@gmail.com>
2025-03-10 10:40:40 -0300

    feat: extend module exports to include lineup and team color types

--- esd/__init__.py
+++ esd/__init__.py
@@ -2,7 +2,29 @@
 EasySoccerData - A Python easy-to-use library for soccer data analysis from multiple sources.
 """
 
-from .sofascore import SofascoreClient, EntityType, Event, Team, TeamEx, Player, MatchStats
+from .sofascore import (
+    SofascoreClient,
+    EntityType,
+    Event,
+    Team,
+    TeamEx,
+    Player,
+    PlayerLineup,
+    MatchStats,
+    Lineups,
+    TeamLineup,
+)
 
-__all__ = ["SofascoreClient", "EntityType", "Event", "Team", "TeamEx", "Player", "MatchStats"]
+__all__ = [
+    "SofascoreClient",
+    "EntityType",
+    "Event",
+    "Team",
+    "TeamEx",
+    "Player",
+    "MatchStats",
+    "Lineups",
+    "TeamLineup",
+    "PlayerLineup",
+]
 __version__ = "0.0.1"
--- esd/sofascore/__init__.py
+++ esd/sofascore/__init__.py
@@ -4,7 +4,7 @@ A submodule for extracting data from Sofascore.
 """
 
 from .client import SofascoreClient
-from .types import EntityType, Event, Team, TeamEx, Player, MatchStats
+from .types import EntityType, Event, Team, TeamEx, Player, MatchStats, PlayerLineup, TeamLineup, Lineups, TeamColor
 
 __all__ = [
     "SofascoreClient",
@@ -14,4 +14,8 @@ __all__ = [
     "TeamEx",
     "Player",
     "MatchStats",
+    "PlayerLineup",
+    "TeamLineup",
+    "Lineups",
+    "TeamColor",
 ]
