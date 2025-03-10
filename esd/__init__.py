224fd6a4105442f4b29b5125de9f81c934d7afc4
Manu <mandrescabral@gmail.com>
2025-03-10 18:32:35 -0300

    feat: add package metadata including description, author, title, and license

--- esd/__init__.py
+++ esd/__init__.py
@@ -28,3 +28,7 @@ __all__ = [
     "PlayerLineup",
 ]
 __version__ = "0.0.1"
+__description__ = "A simple python package for extracting real-time soccer data from diverse online sources, providing essential statistics and insights."
+__author__ = "Manuel Cabral"
+__title__ = "EasySoccerData"
+__license__ = "GPL-3.0"
