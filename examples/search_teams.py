47651a0a7532f6c172ba182857be680a98f71e08
Manu <mandrescabral@gmail.com>
2025-03-10 10:58:34 -0300

    feat: add example script for searching teams

---
+++ examples/search_teams.py
@@ -0,0 +1,25 @@
+"""
+This example shows how to search for teams.
+In this case, we search for teams that contain the word "Liverpool".
+
+The output will be something like:
+
+44, Liverpool, liverpool
+---------------------------
+66774, Liverpool FC Women, liverpool-fc-women
+---------------------------
+6879, Liverpool UY, liverpool-uy
+---------------------------
+.......
+"""
+
+import esd
+
+client = esd.SofascoreClient()
+teams: list[esd.Team] = client.search(
+    "Liverpool", entity=esd.EntityType.TEAM
+)
+
+for team in teams:
+    print(f"{team.id}, {team.name}, {team.slug}")
+    print("---------------------------")
