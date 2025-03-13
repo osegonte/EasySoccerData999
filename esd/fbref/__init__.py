5ddb770389486339838767af6ee70ecd72787c9d
Manu <mandrescabral@gmail.com>
2025-03-13 01:48:43 -0300

    feat: include MatchDetails in FBref module exports

--- esd/fbref/__init__.py
+++ esd/fbref/__init__.py
@@ -3,6 +3,6 @@ FBref client module.
 """
 
 from .client import FBrefClient
-from .types import Match
+from .types import Match, MatchDetails
 
-__all__ = ["FBrefClient", "Match"]
+__all__ = ["FBrefClient", "Match", "MatchDetails"]
