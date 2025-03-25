54f8a354093ee65f4253b18ee7e74fd0a8f96bfa
Manu <mandrescabral@gmail.com>
2025-03-25 00:48:24 -0300

    feat: add example script for retrieving tournament matches

---
+++ examples/promiedos/tournament_matches.py
@@ -0,0 +1,42 @@
+"""
+In this example we will get the matches of a tournament.
+
+Output example:
+
+Champions League uefa-champions-league
+Current filter is Cuartos De Final
+Matches:
+Bayern Munich vs Inter Milan
+Arsenal vs Real Madrid
+FC Barcelona vs Borussia Dortmund
+PSG vs Aston Villa
+Borussia Dortmund vs FC Barcelona
+Aston Villa vs PSG
+Inter Milan vs Bayern Munich
+Real Madrid vs Arsenal
+
+"""
+
+from esd import PromiedosClient, PromiedosTypes
+
+client = PromiedosClient()
+
+# "fhc" is the ID of uefa champions league
+tournament: PromiedosTypes.Tournament = client.get_tournament("fhc")
+
+# print some info
+print(tournament.league.name, tournament.league.slug)
+
+# Get the current filter for getting the matches
+filter: PromiedosTypes.TournamentFilter = tournament.current_filter()
+print("Current filter is", filter.name)
+
+# Get the matches for the tournament
+matches: list[PromiedosTypes.Match] = client.get_tournament_matchs(
+    tournament.league.id, filter.id
+)
+
+# Print the matches
+print("Matches:")
+for match in matches:
+    print(match.home_team.name, "vs", match.away_team.name)
